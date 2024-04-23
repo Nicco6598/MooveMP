65403bee6f68f6c81611a59f9f578d307e62e0c8
Nicco6598 <nicco6598@gmail.com>
2024-04-23 12:21:49 +0200

    Cambio ombre e bottoni per matchare il layout generale

--- nft_frontend/src/pages/OwnedNFTs.tsx
+++ nft_frontend/src/pages/OwnedNFTs.tsx
@@ -42,14 +42,14 @@ const OwnedNFTs: React.FC = () => {
             <h1 className="text-2xl font-bold mb-10">I Miei NFT</h1>
             <div className="grid grid-cols-1 gap-8 md:grid-cols-3 lg:grid-cols-6">
                 {nfts.map(nft => (
-                    <div key={nft.tokenId} className="text-center bg-white rounded-lg shadow-[0px_0px_15px_5px_#edf2f7] hover:shadow-[0px_0px_15px_10px_#BEE3F8] transition-all duration-300 ease-in-out transform hover:scale-105 cursor-pointer p-4">
+                    <div key={nft.tokenId} className="text-center bg-white rounded-lg shadow-[0px_0px_15px_5px_#edf2f7] hover:shadow-[0px_0px_15px_10px_#EBF4FF] transition-all duration-300 ease-in-out transform hover:scale-105 cursor-pointer p-4">
                         <img src={`https://images.unsplash.com/photo-1707344088547-3cf7cea5ca49?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`} alt={`NFT ${nft.tokenId}`} className=" h-auto mb-4 rounded-lg mx-auto" />
                         <p className="text-sm mb-1">
                             <span className="font-bold">Token ID:</span> {nft.tokenId}
                         </p>
                         <p className="text-sm mb-1">
                             <span className="font-bold">Proprietario: </span> 
-                                {`${nft.owner.slice(0, 5)}...${nft.owner.slice(-3)}`}
+                                {`${nft.owner.slice(0, 6)}...${nft.owner.slice(-4)}`}
                         </p>
                         <p className="text-sm mb-1">
                             <span className="font-bold ">Prezzo:</span> {ethers.utils.formatEther(nft.price)} ETH
@@ -63,7 +63,7 @@ const OwnedNFTs: React.FC = () => {
                         <p className="text-sm mb-4">
                             <span className="font-bold">{nft.discountOn}</span> 
                         </p>
-                        <Link to={`/nft/${nft.tokenId}`} className="bg-sky-600 text-white p-2 rounded-lg hover:bg-blue-700 transition-colors duration-300 ease-in-out">
+                        <Link to={`/nft/${nft.tokenId}`} className="bg-sky-500 text-white p-2 rounded-lg hover:bg-blue-700 transition-colors duration-300 ease-in-out">
                                 Visualizza Dettagli
                             </Link>
                     </div>
