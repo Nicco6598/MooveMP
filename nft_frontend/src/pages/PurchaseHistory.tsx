555080407def97666e21be6e3f76725a7b2e5dfc
Nicco6598 <nicco6598@gmail.com>
2024-04-30 15:24:00 +0200

    aggiornata pagina storico acquisti

--- nft_frontend/src/pages/PurchaseHistory.tsx
+++ nft_frontend/src/pages/PurchaseHistory.tsx
@@ -52,10 +52,12 @@ const PurchaseHistory = () => {
     if (loading) return <p className="mt-16 flex flex-col items-center pt-8">Loading...</p>;
 
     return (
-        <div className="p-5">
-            <h1 className="mb-16 flex flex-col items-center pt-8 bg-gradient-to-r from-purple-500 to-sky-500 text-transparent bg-clip-text inline-block">PURCHASE HISTORY</h1>
-            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-5 w-full max-w-3xl">
-                {history.map((item, index) => (
+        <div className="flex justify-center items-center mt-4 flex-col">
+            <h1 className="mb-12 flex flex-col items-center pt-8 bg-gradient-to-r from-purple-500 to-sky-500 text-transparent bg-clip-text inline-block">PURCHASE HISTORY</h1>
+            <div className="grid flex flex-col items-center grid-cols-1 gap-8 mt-5 w-auto max-w-3xl">
+            {history
+                .sort((a, b) => b.timestamp - a.timestamp) // Ordina gli elementi in base al timestamp decrescente
+                .map((item, index) => (
                     <div key={index} className="bg-white rounded-lg shadow-[0px_0px_15px_5px_#edf2f7] hover:shadow-[0px_0px_15px_10px_#EBF4FF] transition-all duration-300 ease-in-out transform hover:scale-105 p-4">
                         <p className="text-gray-700 text-sm font-bold mb-1 truncate">Token ID: {item.tokenId}</p>
                         <p className="text-gray-700 text-sm font-semibold mb-1 truncate">
@@ -64,8 +66,8 @@ const PurchaseHistory = () => {
                         <p className="text-gray-700 text-sm font-semibold mb-4 truncate">
                             Seller: <a href={`https://sepolia.etherscan.io/address/${item.seller}`} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">{item.seller}</a>
                         </p>
-                        <p className="text-gray-700 text-m font-semibold text-purple-700 mb-1 truncate">Price: {ethers.utils.formatEther(item.price)}</p>
-                        <p className="text-gray-500 text-xs bg-gradient-to-r from-purple-500 to-sky-500 text-transparent bg-clip-text inline-block truncate">Date: {new Date(item.timestamp * 1000).toLocaleDateString()}</p>
+                        <p className="text-gray-700 text-m font-semibold text-purple-500 mb-1 truncate">Price: {ethers.utils.formatEther(item.price)}</p>
+                        <p className="text-gray-500 text-sm font-bold bg-gradient-to-r from-red-500 to-purple-500 text-transparent bg-clip-text inline-block truncate">Date: {new Date(item.timestamp * 1000).toLocaleDateString()}</p>
                     </div>
                 ))}
             </div>
